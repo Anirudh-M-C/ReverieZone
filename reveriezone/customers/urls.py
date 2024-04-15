657074eabe4c0ee53b82d9d329c2eb29a6f6c623
Anirudh M <anirudhm394@gmail.com>
2024-04-15 20:50:04 +0530

    account management finished

--- reveriezone/customers/urls.py
+++ reveriezone/customers/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.show_account,name='show_account'),
-
+    path('logout_user/',views.logout_user,name='logout_user'),
 ]
--- reveriezone/customers/views.py
+++ reveriezone/customers/views.py
@@ -2,38 +2,57 @@ from django.shortcuts import render,redirect
 from .state import STATE_CHOICES
 from django.contrib.auth.models import User
 from .models import Customer
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
+
+def logout_user(request):
+     logout(request)
+     return redirect('/')
 def show_account(request):
     if request.POST and 'register' in request.POST:
-        username=request.POST.get('username')
-        password=request.POST.get('password')
-        email=request.POST.get('email')
-        address=request.POST.get('address')
-        phone=request.POST.get('phone')
-        state=request.POST.get('state')
-        city=request.POST.get('city')
-        phone=request.POST.get('phone')
-        location=request.POST.get('location')
-        pincode=request.POST.get('pincode')
-        
-        # create user account
-        user=User.objects.create(
-            username=username,
-            password=password,
-            email=email
-        )
-        #create customer account
-        customer=Customer.objects.create(
-            user=user,
-            phone=phone,
-            address=address,
-            pincode=pincode,
-            location=location,
-            city=city,
-            state=state
-        )
-        return redirect('/')
+        try:
+            username=request.POST.get('username')
+            password=request.POST.get('password')
+            email=request.POST.get('email')
+            address=request.POST.get('address')
+            phone=request.POST.get('phone')
+            state=request.POST.get('state')
+            city=request.POST.get('city')
+            phone=request.POST.get('phone')
+            location=request.POST.get('location')
+            pincode=request.POST.get('pincode')
+            
+            # create user account
+            user=User.objects.create_user(
+                username=username,
+                password=password,
+                email=email
+            )
+            #create customer account
+            customer=Customer.objects.create(
+                user=user,
+                phone=phone,
+                address=address,
+                pincode=pincode,
+                location=location,
+                city=city,
+                state=state
+            )
+            return redirect('/')
+        except Exception as e:
+            error_message=f"duplicate username {e}"
+            messages.error(request,error_message)
+    if request.POST and 'login' in request.POST:
+            username=request.POST.get('username')
+            password=request.POST.get('password')
+            user=authenticate(username=username,password=password)
+            if user:
+                 login(request,user)
+                 return redirect('/')
+            else:
+                messages.error(request,'invalid user credentials')
     context = {
         'STATE_CHOICES': STATE_CHOICES,
     }
